43da9f1bbb5e38f76706b0c6a2bb01e9ad3507da
Ziru Xu <lucy.xu@northwetsern.edu>
2022-06-05 01:14:34 -0500

    Add app.py

--- app.py
+++ app.py
@@ -1,45 +1,140 @@
-"""Configures the subparsers for receiving command line arguments for each
- stage in the model pipeline and orchestrates their execution."""
-import argparse
 import logging.config
+import sqlite3
+import traceback
+import pickle
 
-from config.flaskconfig import SQLALCHEMY_DATABASE_URI
-from src.add_songs import create_db, add_song
+import numpy as np
+import sqlalchemy.exc
+from flask import Flask, render_template, request, redirect, url_for
 
-logging.config.fileConfig('config/logging/local.conf')
-logger = logging.getLogger('penny-lane-pipeline')
+from src.add_bodymeasurement import UserInputManager
+from src.generate_features import extract_features, scale_feature
 
-if __name__ == '__main__':
+# Initialize the Flask application
+app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
 
-    # Add parsers for both creating a database and adding songs to it
-    parser = argparse.ArgumentParser(
-        description="Create and/or add data to database")
-    subparsers = parser.add_subparsers(dest='subparser_name')
+# Configure flask app from flask_config.py
+app.config.from_pyfile("config/flaskconfig.py")
 
-    # Sub-parser for creating a database
-    sp_create = subparsers.add_parser("create_db",
-                                      description="Create database")
-    sp_create.add_argument("--engine_string", default=SQLALCHEMY_DATABASE_URI,
-                           help="SQLAlchemy connection URI for database")
+# Configure logger
+logger = logging.getLogger(__name__)
 
-    # Sub-parser for ingesting new data
-    sp_ingest = subparsers.add_parser("ingest",
-                                      description="Add data to database")
-    sp_ingest.add_argument("--artist", default="Emancipator",
-                           help="Artist of song to be added")
-    sp_ingest.add_argument("--title", default="Minor Cause",
-                           help="Title of song to be added")
-    sp_ingest.add_argument("--album", default="Dusk to Dawn",
-                           help="Album of song being added")
-    sp_ingest.add_argument("--engine_string",
-                           default='sqlite:///data/tracks.db',
-                           help="SQLAlchemy connection URI for database")
+# Initialize the database session
+application_manager = UserInputManager(app)
 
-    args = parser.parse_args()
-    sp_used = args.subparser_name
-    if sp_used == 'create_db':
-        create_db(args.engine_string)
-    elif sp_used == 'ingest':
-        add_song(args)
-    else:
-        parser.print_help()
+try:
+    model_path = "models/Lasso.sav"
+    model = pickle.load(open(model_path, 'rb'))
+except FileNotFoundError as e:
+    logger.error("The model file does not exist at " + model_path)
+    raise e
+else:
+    logger.info("Successfully load the model from " + model_path)
+
+try:
+    scaler_path = "models/scaler.sav"
+    scaler = pickle.load(open(scaler_path, 'rb'))
+except FileNotFoundError as e:
+    logger.error("The scalar file does not exist at " + scaler_path)
+    raise e
+else:
+    logger.info("Successfully load the scalar from " + scaler_path)
+
+
+@app.route('/')
+def index():
+    try:
+        logger.debug("Index page accessed")
+        return render_template('index.html')
+    except:
+        traceback.print_exc()
+        logger.warning("Not able to display loan applications information, error page returned")
+        return render_template('error.html')
+
+
+@app.route('/result', methods=['POST', 'GET'])
+def add_entry():
+    """View that process a POST with new applicant input
+    Add new applicant information to Applications database and get prediction results
+    Returns:
+        rendered html template located at: app/templates/result.html if successfully processed,
+        rendered html template located at: app/templates/error.html if any error occurs
+    """
+    if request.method == 'GET':
+        return "Visit the homepage to add applicants and get predictions"
+    elif request.method == 'POST':
+        try:
+            # Add user body information to RDS for future usages
+            application_manager.add_user(
+                name=request.form['name'],
+                age=request.form['age'],
+                height=request.form['height'],
+                weight=request.form['weight'],
+                neck=request.form['neck'],
+                chest=request.form['chest'],
+                abdomen=request.form['abdomen'],
+                hip=request.form['hip'],
+                thigh=request.form['thigh'],
+                knee=request.form['knee'],
+                ankle=request.form['ankle'],
+                biceps=request.form['biceps'],
+                forearm=request.form['forearm'],
+                wrist=request.form['wrist']
+            )
+
+            logger.info("New user body measurement is added")
+
+            age = request.form['age']
+            weight = request.form['weight']
+            height = request.form['height']
+            neck = request.form['neck']
+            chest = request.form['chest']
+            abdomen = request.form['abdomen']
+            hip = request.form['hip']
+            thigh = request.form['thigh']
+            knee = request.form['knee']
+            ankle = request.form['ankle']
+            biceps = request.form['biceps']
+            forearm = request.form['forearm']
+            wrist = request.form['wrist']
+            user_input = [age, weight, height, neck, chest, abdomen, hip, thigh, knee, ankle, biceps, forearm, wrist]
+            user_input = np.array(user_input).reshape(1,-1)
+            user_input_transformed = scaler.transform(user_input)
+            user_prediction = float(model.predict(user_input_transformed))
+            user_prediction = round(user_prediction, 1)
+            percentage = user_prediction * 7
+            if user_prediction < 8:
+                body_percentage = 20
+            elif 8 <= user_prediction < 15:
+                body_percentage = 82
+            elif 15 <= user_prediction <= 18:
+                body_percentage = 144
+            elif 18 < user_prediction < 25:
+                body_percentage = 206
+            elif user_prediction >= 30:
+                body_percentage = 268
+
+
+            logger.info("The predicted body fat for the user is %s", user_prediction)
+
+            logger.debug("Result page accessed")
+
+            return render_template('index.html', user_prediction=user_prediction, percentage=percentage, body_percentage=body_percentage)
+
+            #return redirect(url_for('index'), user_prediction=user_prediction, percentage=percentage, body_percentage=body_percentage)
+        except sqlite3.OperationalError as e:
+            logger.error(
+                "Error page returned. Not able to add song to local sqlite "
+                "database: %s. Error: %s ",
+                app.config['SQLALCHEMY_DATABASE_URI'], e)
+            return render_template('error.html')
+        except sqlalchemy.exc.OperationalError as e:
+            logger.error(
+                "Error page returned. Not able to add song to MySQL database: %s. "
+                "Error: %s ",
+                app.config['SQLALCHEMY_DATABASE_URI'], e)
+            return render_template('error.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
