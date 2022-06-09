0e0d30768f79058ad3abc63a869dfe67a3f10d36
Ziru Xu <lucy.xu@northwetsern.edu>
2022-06-09 12:16:56 -0500

    Change pylint

--- src/s3.py
+++ src/s3.py
@@ -1,11 +1,9 @@
-import argparse
 import logging.config
 import re
 import typing
 
 import boto3
 import botocore
-import pandas as pd
 
 # Adapted from: https://github.com/MSIA/2022-msia423/blob/main/aws-s3/s3.py
 
@@ -23,9 +21,9 @@ def parse_s3(s3path: str) -> typing.Tuple[str, str]:
        """
     regex = r"s3://([\w._-]+)/([\w./_-]+)"
 
-    m = re.match(regex, s3path)
-    s3bucket = m.group(1)
-    s3path = m.group(2)
+    pattern = re.match(regex, s3path)
+    s3bucket = pattern.group(1)
+    s3path = pattern.group(2)
 
     return s3bucket, s3path
 
@@ -40,15 +38,16 @@ def upload_file_to_s3(local_path: str, s3path: str) -> None:
        """
     s3bucket, s3_just_path = parse_s3(s3path)
 
-    s3 = boto3.resource("s3")
-    bucket = s3.Bucket(s3bucket)
+    s_3 = boto3.resource("s3")
+    bucket = s_3.Bucket(s3bucket)
 
     try:
         bucket.upload_file(local_path, s3_just_path)
-    except botocore.exceptions.NoCredentialsError:
-        logger.error('Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
+    except botocore.exceptions.NoCredentialsError as e:
+        logger.error("Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.")
+        raise e
     else:
-        logger.info('Data uploaded from %s to %s', local_path, s3path)
+        logger.info("Data uploaded from %s to %s", local_path, s3path)
 
 
 def download_file_from_s3(local_path: str, s3path: str) -> None:
@@ -61,12 +60,13 @@ def download_file_from_s3(local_path: str, s3path: str) -> None:
         """
     s3bucket, s3_just_path = parse_s3(s3path)
 
-    s3 = boto3.resource("s3")
-    bucket = s3.Bucket(s3bucket)
+    s_3 = boto3.resource("s3")
+    bucket = s_3.Bucket(s3bucket)
 
     try:
         bucket.download_file(s3_just_path, local_path)
-    except botocore.exceptions.NoCredentialsError:
-        logger.error('Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.')
+    except botocore.exceptions.NoCredentialsError as e:
+        logger.error("Please provide AWS credentials via AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY env variables.")
+        raise e
     else:
-        logger.info('Data downloaded from %s to %s', s3path, local_path)
+        logger.info("Data downloaded from %s to %s", s3path, local_path)
